7039454d53fee42ad862dab73b2f09d9bbedb782
Phiretom <145915399+Phiretom@users.noreply.github.com>
2023-11-06 21:03:57 +0300

    fixed typo in class name for Circle unittests added rectangle unittests

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,4 @@
-
+import unittest
 
 def area(a, b):
     """Получает на вход боковую сторону a и основание прямоугольника b, возвращает его площадь"""
@@ -7,3 +7,29 @@ def area(a, b):
 def perimeter(a, b):
     """Получает на вход боковую сторону a и основание прямоугольника b, возвращает его периметр"""
     return 2*(a + b)
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(0, 12)
+        self.assertEqual(res, 0)
+
+    def test_one_mul(self):
+        res = area(10, 13)
+        self.assertEqual(res, 130)
+
+    def test_two_mul(self):
+        res = area(67.5, 3.5)
+        self.assertEqual(res, 236.25)
+
+    def test_three_mul(self):
+        res = perimeter(1, 6)
+        self.assertEqual(res, 14)
+
+    def test_four_mul(self):
+        res = perimeter(10, 54)
+        self.assertEqual(res, 128)
+
+    def test_five_mul(self):
+        res = perimeter(67.636, 42.364)
+        self.assertEqual(res, 220)
