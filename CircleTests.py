813c436b4f61ac09219bec6c451c8d3ded56e09b
Phiretom <145915399+Phiretom@users.noreply.github.com>
2023-11-08 15:56:15 +0300

    moved Circle unittest to different file

---
+++ CircleTests.py
@@ -0,0 +1,28 @@
+import unittest
+from circle import area, perimeter
+
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_one_mul(self):
+        res = area(10)
+        self.assertEqual(res, 314.1592653589793)
+
+    def test_two_mul(self):
+        res = area(67.636)
+        self.assertEqual(res, 14371.619275936122)
+
+    def test_three_mul(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_four_mul(self):
+        res = perimeter(10)
+        self.assertEqual(res, 62.83185307179586)
+
+    def test_five_mul(self):
+        res = perimeter(67.636)
+        self.assertEqual(res, 424.96952143639845)
